eda860a352a3eba63a86c39e26688f0d4a2d25c5
robonexx <robertwagar@gmail.com>
2022-05-14 16:47:05 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-     Street ART
+      <ul>
+        <li> <h1>Street Art Market</h1></li>
+        <li> <h1>Street Art Market</h1></li>
+        <li> <h1>Street Art Market</h1></li>
+        <li> <h1>Street Art Market</h1></li>
+        <li> <h1>Street Art Market</h1></li>
+        <li> <h1>Street Art Market</h1></li>
+        <li> <h1>Street Art Market</h1></li>
+        <li> <h1>Street Art Market</h1></li>
+        <li> <h1>Street Art Market</h1></li>
+        <li> <h1>Street Art Market</h1></li>
+      </ul>
+    <h1 className='special'>Street Art Market</h1>
     </div>
   );
 }
